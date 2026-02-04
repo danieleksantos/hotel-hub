5c2734c5cd2199abc9edcfc688206db4ab141430
Daniele Karina dos Santos <danimistron@gmail.com>
2026-02-04 08:11:09 -0300

    fix: config docker

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -4,14 +4,15 @@ const api = axios.create({
   baseURL: (import.meta.env.VITE_API_URL as string) || 'http://localhost:3000',
 })
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('@HotelHub:token')
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`
-  }
-
-  return config
-})
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      console.warn('⚠️ Sessão expirada ou token inválido. Redirecionando...')
+      localStorage.removeItem('@HotelHub:token')
+    }
+    return Promise.reject(error)
+  },
+)
 
 export default api
